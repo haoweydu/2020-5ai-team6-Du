1fa29821251bbdc8031fc9c3b79c7b538cb705bd
haoweydu <haoweyduscuola@gmail.com>
2019-12-18 11:15:48 +0100

    html è almeno presentabile

--- html/Chart.js
+++ html/Chart.js
@@ -9,35 +9,37 @@ google.charts.setOnLoadCallback(drawChart);
 // draws it.
 function drawChart() {
 
-
+    // Create the data table.
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['periodo silenzioso',     11],
+        ['periodo di media',      5],
+        ['periodo rumoroso',      2]
+    ]);
     var data1 = google.visualization.arrayToDataTable([
-        ['Year', 'Sales', 'Expenses'],
+        ['Year', 'aula1', 'aula2'],
         ['2013', 1000, 400],
         ['2014', 1170, 460],
         ['2015', 660, 1120],
         ['2016', 1030, 540]
     ]);
     var options1 = {
-        title: 'Company Perfce',
+        title: 'livelli rumore',
         hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
         vAxis: { minValue: 0 }
     };
     var chart1 = new google.visualization.AreaChart(document.getElementById('chart_div1'));
     chart1.draw(data1, options1);
-    // Create the data table.
-    var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['periodo silenzioso', 11],
-        ['periodo di media', 5],
-        ['periodo rumoroso', 2]
-    ]);
+
+    // Set chart options
     var options = {
         title: 'Confronto livelli rumore',
-        slices: [{color: "#006400"},{color: '#FFD300'},{color: '#FF0000'}]
+        slices: [{color: "#006400"},{color: '#FFD300'},{color: 'red'}]
     };
 
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
     chart.draw(data, options);
     
+
 }
